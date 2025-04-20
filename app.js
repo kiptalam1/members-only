76eca749a8c46bcd603a040cf09a5f719b26cc6a
adams <adamskiptalam0@gmail.com>
2025-04-20 14:32:03 +0300

    Initial basic app set up

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import session from "express-session";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config();
+const PORT = 3000;
+
+const app = express();
+
+// set up view engine and ejs
+app.set("view engine", "ejs");
+app.set(express.urlencoded({ extended: false }));
+
+//set up files paths
+app.use(express.static(path.join(process.cwd(), "public")));
+// set views directory.
+app.set("views", path.join(process.cwd(), "views"));
+//set app session middleware.
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+app.use(passport.session());
+
+// routes
+app.get("/", (req, res, next) => {
+	res.render("index");
+});
+
+app.listen(PORT, () => {
+	console.log(`http://localhost:${PORT}`);
+});
